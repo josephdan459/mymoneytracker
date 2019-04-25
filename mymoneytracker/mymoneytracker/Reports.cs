5082bf7c42c8f6f85b64917bd0b0b3733f1e9b62
robkau <robkau@github.com>
2019-04-24 23:30:11 -0400

    add category summaries to report

--- mymoneytracker/mymoneytracker/Reports.cs
+++ mymoneytracker/mymoneytracker/Reports.cs
@@ -18,38 +18,54 @@ namespace mymoneytracker
                 File.Delete(reportPath);
             }
 
-            var fp = new FileInfo(reportPath);
-
-            // get all distinct categories
-            List<string> categories = transactions.Select(t => t.Category).Distinct().ToList();
-
-            // get gain/loss for each category
+            var fp = new FileInfo(reportPath);           
 
             using (var f = new ExcelPackage(fp))
             {
                 var ws = f.Workbook.Worksheets.Add($"My {reportDays}-day Budget Report");
 
 
+                /////
                 // category summmaries - column showing gain/loss per category over X days
-                ws.Cells["A1:B1"].Merge = true;
-                ws.Cells["A1:B1"].Style.HorizontalAlignment = OfficeOpenXml.Style.ExcelHorizontalAlignment.Center;
-                ws.Cells["A1:B1"].Value = "Category Summaries";
-                ws.Cells["A1:B1"].Style.Font.Bold = true;
+                ws.Cells[1, 1, 1, 2].Merge = true;
+                ws.Cells[1, 1, 1, 2].Style.HorizontalAlignment = OfficeOpenXml.Style.ExcelHorizontalAlignment.Center;
+                ws.Cells[1, 1, 1, 2].Value = "Category Summaries";
+                ws.Cells[1, 1, 1, 2].Style.Font.Bold = true;
+                ws.Cells[2,1].Value = "Category";
+                ws.Cells[2,2].Value = "Gain/Loss";
+                ws.Cells[2,1,2,2].Style.Font.Bold = true;
 
-                ws.Cells["A2"].Value = "Category";
-                ws.Cells["B2"].Value = "Gain/Loss";
-                ws.Cells["A2:B2"].Style.Font.Bold = true;
+                // get all distinct categories
+                DateTime cutoffDate = DateTime.Now.AddDays(-reportDays);
+                List<string> categories = transactions.Where(t => (t.Date.CompareTo(cutoffDate) > 0)).Select(t => t.Category).Distinct().ToList();
                 
-                ws.Cells["A3"].Value = "testCategory";
-                ws.Cells["B3"].Value = "$-1000";
-                
+                int i = 3;
+                foreach (var category in categories)
+                {
+                    // sum the changes over last X days                    
+                    decimal changed = (from t in transactions where ((t.Category == category) && (t.Date.CompareTo(cutoffDate) >= 0)) select t.Amount).Sum();
 
+                    var c = category;
+                    if (category == "")
+                    {
+                        c = "Uncategorized";
+                    }
+
+                    ws.Cells[i, 1].Value = c;
+                    ws.Cells[i, 2].Value = changed;
+                    i++;
+                }
+
+                /////
                 // balance graph - show balance per day over last X days
 
+                /////
                 // show top 10 largest expenses in last X days
 
+                /////
                 // line graph sources of inflow over X days
 
+                /////
                 // line graph sources of outflow over X days
 
 
@@ -59,6 +75,6 @@ namespace mymoneytracker
                 ws.Column(2).Width = 20;
                 f.Save();                
             }
-        }        
+        }      
     }
 }
