e03197032d014dc94e34b44a176a7916bbd66bac
robkau <robkau@github.com>
2019-04-24 20:20:50 -0400

    Add basic unit test for transaction model

---
+++ mymoneytracker/mymoneytracker-test/Test_Transactions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace mymoneytracker_test
+{
+    [TestClass]
+    public class Test_Transactions
+    {
+        [TestMethod]
+        public void TestNewTransactionDefaultValues()
+        {
+            var t = new mymoneytracker.TransactionModel();
+
+            Assert.IsNotNull(t);
+            Assert.AreEqual(t.Payee, "Payee");
+        }
+    }
+}
