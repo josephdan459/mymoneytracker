310855892d8a4420a375bdec135bdff574556cb2
robkau <robkau@github.com>
2019-04-26 21:02:52 -0400

    add top 10 most expensive purchases to report

--- mymoneytracker/mymoneytracker/Reports.cs
+++ mymoneytracker/mymoneytracker/Reports.cs
@@ -6,6 +6,8 @@ using System.Text;
 using System.Threading.Tasks;
 
 using OfficeOpenXml;
+using OfficeOpenXml.Drawing.Chart;
+using OfficeOpenXml.Style;
 
 namespace mymoneytracker
 {
@@ -16,10 +18,8 @@ namespace mymoneytracker
 
             var bgOffset = 0;
             var i = 0;
-            if (ShowCategorySummaries)
-            {
-                bgOffset += 2;
-            }
+
+
 
             if (File.Exists(reportPath))
             {
@@ -67,39 +67,93 @@ namespace mymoneytracker
                         ws.Cells[i, 2].Value = changed;
                         i++;
                     }
+
+                    ws.Column(1).Width = 20;
+                    ws.Column(2).Width = 20;
+
+                    bgOffset += 3;
                 }
 
                 /////
                 // balance graph - show balance per day over last X days
-                if (ShowBalanceGraph)
+                if (ShowBalanceGraph && false)
                 {
                     var bgCol = 1 + bgOffset;
                     var r = 10;
                     i = 1;
-                    ws.Cells[r, bgCol].Value = currentBalance;
                     foreach (var t in transactions.Where(t => (t.Date.CompareTo(cutoffDate) >= 0)).ToList())
                     {
-                        ws.Cells[r + i, bgCol].Value = t.Balance;
+                        ws.Cells[r + i, bgCol].Value = i;
+                        ws.Cells[r + i, bgCol + 1].Value = t.Balance;
                         i++;
                     }
+
+                    ExcelChart lineChart = ws.Drawings.AddChart("lineChart", eChartType.Line);
+                    lineChart.Title.Text = "Account Balance";
+
+                    var balanceRange = ws.Cells[r + 1, bgCol, r + 1 + transactions.Where(t => (t.Date.CompareTo(cutoffDate) >= 0)).Count() - 1, bgCol];
+                    var balanceAmounts = ws.Cells[r + 1, bgCol + 1, r + 1 + transactions.Where(t => (t.Date.CompareTo(cutoffDate) >= 0)).Count() - 1, bgCol + 1];
+                    balanceRange.Style.Border.Top.Style = ExcelBorderStyle.Thin;
+                    balanceAmounts.Style.Border.Bottom.Style = ExcelBorderStyle.Thin;
+
+                    var bs = (ExcelLineChartSerie)lineChart.Series.Add(balanceAmounts, balanceRange);
+                    //bs.Header = "Account Balance";
+
+                    //size of the chart
+                    lineChart.SetSize(600, 300);
+
+                    //add the chart at cell B6
+                    lineChart.SetPosition(5, 0, 1, 0);
+
+                    bgOffset += 3;
+
+
                 }
 
                 /////
                 // show top 10 largest expenses in last X days
                 if (ShowMostExpensivePurchases)
                 {
+                    var bgCol = 3 + bgOffset;
+                    var r = 4;
 
+                    ws.Cells[r, bgCol, r, bgCol + 2].Merge = true;
+                    ws.Cells[r, bgCol, r, bgCol + 2].Style.HorizontalAlignment = OfficeOpenXml.Style.ExcelHorizontalAlignment.Center;
+                    ws.Cells[r, bgCol, r, bgCol + 2].Value = "Largest Expenses";
+                    ws.Cells[r, bgCol, r, bgCol + 2].Style.Font.Bold = true;
+
+                    r += 1;
+                    ws.Cells[r, bgCol].Value = "Payee";
+                    ws.Cells[r, bgCol + 1].Value = "Amount";
+                    ws.Cells[r, bgCol + 2].Value = "Category";
+
+                    List<TransactionModel> mostExpensive = transactions.Where(t => t.Amount < 0).OrderBy(t => t.Amount).Take(10).ToList();                    
+                    i = 1;
+                    foreach (var t in mostExpensive)
+                    {
+                        ws.Cells[r + i, bgCol].Value = t.Payee;
+                        ws.Cells[r + i, bgCol + 1].Value = t.Amount;
+                        ws.Cells[r + i, bgCol + 2].Value = t.Category;
+                        i++;
+                    }
+
+
+                    ws.Column(bgCol).Width = 15;
+                    ws.Column(bgCol + 1).Width = 15;
+                    ws.Column(bgCol + 2).Width = 15;
+
+                    bgOffset += 3;
                 }
 
                 /////
-                // line graph sources of inflow over X days
+                // pie graph sources of inflow over X days
                 if (ReportInflowGraph)
                 {
 
                 }
 
                 /////
-                // line graph sources of outflow over X days
+                // pie graph sources of outflow over X days
                 if (ReportOutflowGraph)
                 {
 
@@ -107,10 +161,8 @@ namespace mymoneytracker
 
 
 
-                ws.Column(1).Width = 20;
-                ws.Column(2).Width = 20;
-                f.Save();                
+                f.Save();
             }
-        }      
+        }
     }
 }
